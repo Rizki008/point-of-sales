27a737aeb18264a9de6eaf15e7b540a7cb60f75b
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-02-06 09:38:33 +0700

    menambahkan file jtsream untuk login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.auth')
+@section('login')
+
+    <div class="login-box">
+
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <div class="login-logo">
+                    <a href="{{ url('/') }}"><img src="{{ asset('img/download.png') }}" alt="download.png"
+                            width="100"></a>
+                </div>
+
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
+                    <div class="input-group mb-3  @error('email') has-error @enderror">
+                        <input type="email" name="email" class="form-control" placeholder="Email" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                                @error('email')
+                                    <span class="help-block">{{ $message }}</span>
+                                @enderror
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3 @error('password')
+
+                    @enderror">
+                        <input type="password" name="password" class="form-control" placeholder="Password" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                                @error('password')
+                                    <span class="help-block">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+@endsection
