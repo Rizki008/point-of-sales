0c17c3abe27edd533043d4422dbae62ae96b9fab
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-02-06 19:35:04 +0700

    menambahkan script js add edit dan delete

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -15,8 +15,8 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <button onclick="addForm()" type="button" class="btn btn-primary btn-sm"><i
-                                class="fa fa-plus"></i>
+                        <button onclick="addForm('{{ route('kategori.store') }}')" type="button"
+                            class="btn btn-primary btn-sm"><i class="fa fa-plus"></i>
                             Add</button>
                     </div>
                     <!-- /.card-header -->
@@ -50,19 +50,88 @@
             table = $('.table').DataTable({
                 processing: true,
                 autoWidth: false,
-                // ajax: {
-                //     url: '{{ route('kategori.data') }}',
-                // }
+                ajax: {
+                    url: '{{ route('kategori.data') }}',
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'nama_kategori'
+                    },
+                    {
+                        data: 'aksi',
+                        searchable: false,
+                        sortable: false
+                    }
+                ]
             });
+
+            $('#modal-form').validator().on('submit', function(e) {
+                if (!e.preventDefault()) {
+                    $.ajax({
+                            url: $('#modal-form form').attr('action'),
+                            type: 'post',
+                            data: $('#modal-form form').serialize()
+                        })
+                        .done((response) => {
+                            $('#modal-form').modal('hide');
+                            table.ajax.reload();
+                        })
+                        .fail((errors) => {
+                            alert('Tidak Dapat Menambahkan Data');
+                            return;
+                        });
+                }
+            })
         });
 
-        function addForm() {
+        function addForm(url) {
             $('#modal-form').modal('show');
             $('#modal-form .modal-title').text('Tambah Kategori');
 
             $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
             $('#modal-form [name=_method]').val('post');
             $('#modal-form [name=nama_kategori]').focus();
+        }
+
+        function editForm(url) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Edit Kategori');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('put');
+            $('#modal-form [name=nama_kategori]').focus();
+
+            $.get(url)
+                .done((response) => {
+                    $('#modal-form [name=nama_kategori]').val(response.nama_kategori);
+                })
+                .fail((errors) => {
+                    alert('Tidak Dapat Menampilkan Data');
+                    return;
+                });
+        }
+
+        function deleteData(url) {
+            if (confirm('Apakah Yakin Akan Menghapus Data??')) {
+                $.post(url, {
+                        // '_token': '{{ csrf_token() }}'
+                        '_token': $('[name=csrf-token]').attr('content'),
+                        '_method': 'delete'
+                    })
+                    .done((response) => {
+                        table.ajax.reload();
+                    })
+                    .fail((errors) => {
+                        alert('Tidak Dapat Menghapus Data');
+                        return;
+                    });
+            }
         }
     </script>
 @endpush
