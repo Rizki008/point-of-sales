909a7c846855d87c05b6cb6bf2738b07c0270480
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-02-07 08:46:56 +0700

    menambahkan script js add,edit,delete produk

---
+++ resources/views/produk/index.blade.php
@@ -0,0 +1,169 @@
+@extends('layout.master')
+
+@section('title')
+    Daftar Produk
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="breadcrumb-item active">Daftar Produk</li>
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <button onclick="addForm('{{ route('produk.store') }}')" type="button"
+                            class="btn btn-primary btn-sm"><i class="fa fa-plus"></i>
+                            Add</button>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th width="5%">No</th>
+                                    <th>Kode Produk</th>
+                                    <th>Nama Produk</th>
+                                    <th>Kategoori</th>
+                                    <th>Merek</th>
+                                    <th>Harga Beli</th>
+                                    <th>Harga Jual</th>
+                                    <th>Diskon</th>
+                                    <th>Stock</th>
+                                    <th width="15%"><i class="fa fa-cog"></i></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+    </div>
+    @includeIf('produk.form')
+@endsection
+
+@push('scripts')
+    <script>
+        let table;
+        $(function() {
+            table = $('.table').DataTable({
+                processing: true,
+                autoWidth: false,
+                ajax: {
+                    url: '{{ route('produk.data') }}',
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'kode_produk'
+                    },
+                    {
+                        data: 'nama_produk'
+                    },
+                    {
+                        data: 'nama_kategori'
+                    },
+                    {
+                        data: 'merek'
+                    },
+                    {
+                        data: 'harga_beli'
+                    },
+                    {
+                        data: 'harga_jual'
+                    },
+                    {
+                        data: 'diskon'
+                    },
+                    {
+                        data: 'stock'
+                    },
+                    {
+                        data: 'aksi',
+                        searchable: false,
+                        sortable: false
+                    }
+                ]
+            });
+
+            $('#modal-form').validator().on('submit', function(e) {
+                if (!e.preventDefault()) {
+                    $.ajax({
+                            url: $('#modal-form form').attr('action'),
+                            type: 'post',
+                            data: $('#modal-form form').serialize()
+                        })
+                        .done((response) => {
+                            $('#modal-form').modal('hide');
+                            table.ajax.reload();
+                        })
+                        .fail((errors) => {
+                            alert('Tidak Dapat Menambahkan Data');
+                            return;
+                        });
+                }
+            })
+        });
+
+        function addForm(url) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Tambah Produk');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('post');
+            $('#modal-form [name=nama_produk]').focus();
+        }
+
+        function editForm(url) {
+            $('#moda-form').modal('show');
+            $('#modal-form .modal-title').text('Edit Produk');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('put');
+            $('#modal-fotm [name=nama_produk]').focus();
+
+            $.get(url)
+                .done((response) => {
+                    $('#modal-form [name=nama_produk]').val(response.nama_produk);
+                    $('#modal-form [name=id_kategori]').val(response.id_kategori);
+                    $('#modal-form [name=merek]').val(response.merek);
+                    $('#modal-form [name=harga_beli]').val(response.harga_beli);
+                    $('#modal-form [name=harga_jual]').val(response.harga_jual);
+                    $('#modal-form [name=diskon]').val(response.diskon);
+                    $('#modal-form [name=stock]').val(response.stock);
+                })
+                .fail((errors) => {
+                    alert('Tidak Dapat Menampilkan Data');
+                    return;
+                });
+        }
+
+        function deleteData(url) {
+            if (confirm('Apakah Yakin Akan menghapus data Produk?')) {
+                $.post(url, {
+                        '_token': $('[name=csrf-token]').attr('content'),
+                        '_method': 'delete'
+                    })
+                    .done((response) => {
+                        table.ajax.reload();
+                    })
+                    .fail((errors) => {
+                        alert('Tidak Dapat Menghapus data');
+                        return;
+                    });
+            }
+        }
+    </script>
+@endpush
