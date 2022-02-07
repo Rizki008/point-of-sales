7f9331a34908f95a4a5340072f33c141d319eafb
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-02-07 08:40:39 +0700

    menambahkan sript crud produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
+use App\Models\Produk;
 
 class ProdukController extends Controller
 {
@@ -13,9 +15,43 @@ class ProdukController extends Controller
      */
     public function index()
     {
-        //
+        $kategori = Kategori::all()->pluck('nama_kategori', 'id_kategori');
+        return view('produk.index', compact('kategori'));
     }
 
+    public function data()
+    {
+        $produk = Produk::leftJoin('kategori', 'kategori.id_kategori', 'produk.id_kategori')
+            ->select('produk.*', 'nama_kategori')
+            ->orderBy('kode_produk', 'asc')
+            ->get();
+
+        return datatables()
+            ->of($produk)
+            ->addIndexColumn()
+            ->addColumn('kode_produk', function ($produk) {
+                return '<span class="badge badge-success">' . $produk->kode_produk . '</span>';
+            })
+            ->addColumn('harga_beli', function ($produk) {
+                return format_uang($produk->harga_beli);
+            })
+            ->addColumn('harga_jual', function ($produk) {
+                return format_uang($produk->harga_jual);
+            })
+            ->addColumn('stock', function ($produk) {
+                return format_uang($produk->stock);
+            })
+            ->addColumn('aksi', function ($produk) {
+                return '
+                <div class="btn-grup">
+                    <button onclick="editForm(`' . route('produk.update', $produk->id_produk) . '`)" class="btn btn-sm btn-warning"> <i class="fa fa-edit"></i></button>
+                    <button onclick="deleteData(`' . route('produk.destroy', $produk->id_produk) . '`)" class="btn btn-sm btn-danger"> <i class="fa fa-trash"></i></button>
+                </div>
+                ';
+            })
+            ->rawColumns(['aksi', 'kode_produk'])
+            ->make(true);
+    }
     /**
      * Show the form for creating a new resource.
      *
@@ -34,7 +70,19 @@ class ProdukController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $produk = Produk::latest()->first();
+        $request['kode_produk'] = 'P' . tambah_nol_didepan((int)$produk->id_produk + 1, 6);
+
+        //cara simpel menggunakan eloquen
+        $produk = Produk::create($request->all());
+
+        //cara panjang
+        // $produk = new Produk();
+        // $produk->nama_produk = $request->nama_produk;
+        // $produk->save();
+
+        return response()->json('Data Berhsil Disimpan', 200);
     }
 
     /**
@@ -45,7 +93,9 @@ class ProdukController extends Controller
      */
     public function show($id)
     {
-        //
+        $produk = Produk::find($id);
+
+        return response()->json($produk);
     }
 
     /**
@@ -68,7 +118,11 @@ class ProdukController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $$produk = Produk::find($id);
+        //cara simpel menggunakan eloquen
+        $produk->update($request->all());
+
+        return response()->json('Data Berhasil Diupdate', 200);
     }
 
     /**
@@ -79,6 +133,9 @@ class ProdukController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $produk = Produk::find($id);
+        $produk->delete();
+
+        return response(null, 204);
     }
 }
