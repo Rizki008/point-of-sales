20744bd1d79286272653c454eaf56562c52b0f10
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-02-09 20:16:52 +0700

    menambahkan script deleteselected

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -29,6 +29,11 @@ class ProdukController extends Controller
         return datatables()
             ->of($produk)
             ->addIndexColumn()
+            ->addColumn('select_all', function ($produk) {
+                return '
+                <input type="checkbox" name="id_produk[]" value="' . $produk->id_produk . '">
+                ';
+            })
             ->addColumn('kode_produk', function ($produk) {
                 return '<span class="badge badge-success">' . $produk->kode_produk . '</span>';
             })
@@ -49,7 +54,7 @@ class ProdukController extends Controller
                 </div>
                 ';
             })
-            ->rawColumns(['aksi', 'kode_produk'])
+            ->rawColumns(['aksi', 'kode_produk', 'select_all'])
             ->make(true);
     }
     /**
@@ -136,6 +141,17 @@ class ProdukController extends Controller
         $produk = Produk::find($id);
         $produk->delete();
 
+        return response(null, 204);
+    }
+
+    public function deleteSelected(Request $request)
+    {
+        // return $request;
+        foreach ($request->id_produk as $id) {
+            $produk = Produk::find($id);
+            $produk->delete();
+            // return $request->id_produk[$key];
+        }
         return response(null, 204);
     }
 }
