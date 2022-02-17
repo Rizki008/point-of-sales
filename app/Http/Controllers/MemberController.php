d72341ef9163038132b010e0f596135c7d6cb8b0
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-02-17 16:03:32 +0700

    menambahkan script member controller

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -14,9 +14,27 @@ class MemberController extends Controller
      */
     public function index()
     {
-        //
+        return view('member.index');
     }
 
+    public function data()
+    {
+        $member = Member::orderBy('id_member', 'desc')->get();
+
+        return datatables()
+            ->of($member)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($member) {
+                return '
+            <div class="btn-group>
+            <button onclick="editForm(`' . route('member.update', $member->id_member) . '`)" class="btn btn-warning btn-sm><i class="fa fa-edit"></i></button>
+            <button onclick="deleteData(`' . route('member.destroy', $member->id_member) . '`)" class="btn btn-warning btn-sm><i class="fa fa-edit"></i></button>
+            </div>
+            ';
+            })
+            ->rowColumns(['aksi'])
+            ->make(true);
+    }
     /**
      * Show the form for creating a new resource.
      *
@@ -35,27 +53,33 @@ class MemberController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $member = new Member();
+        $member->nama_member = $request->nama_member;
+        $member->save();
+
+        return response()->json('Data berhasil disimpan', 200);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Member  $member
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Member $member)
+    public function show(Member $id)
     {
-        //
+        $member = Member::find($id);
+
+        return response()->json($member);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Member  $member
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Member $member)
+    public function edit(Member $id)
     {
         //
     }
@@ -64,22 +88,29 @@ class MemberController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Member  $member
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Member $member)
+    public function update(Request $request, Member $id)
     {
-        //
+        $member = Member::find($id);
+        $member->nama_member = $request->nama_member;
+        $member->update();
+
+        return response()->json('Data Berhasil Diupdate', 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Member  $member
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Member $member)
+    public function destroy(Member $id)
     {
-        //
+        $member = Member::find($id);
+        $member->delete();
+
+        return response(null, 204);
     }
 }
