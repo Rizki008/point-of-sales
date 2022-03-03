b56adf4edd805d8037e2e7c9e62c18a9f7b3a3cd
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-03-03 07:18:25 +0700

    mengedit db setting

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SettingTableSeeder extends Seeder
 {
@@ -13,6 +14,15 @@ class SettingTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('setting')->insert([
+            'id_setting' => 1,
+            'nama_prusahaan' => 'Pont of sales',
+            'alamat' => 'Kuningan jl.siliwangi no.56',
+            'telepon' => '085731639595',
+            'tipe_nota' => 1, //kecil
+            'diskon' => 5,
+            'path_logo' => '/img/logo.png',
+            'path_kartu_member' => '/img/member.png',
+        ]);
     }
 }
