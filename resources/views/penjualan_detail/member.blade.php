b8d695ff7ace7a5bf37a53697a1da8b0d2a3d7fd
muhammad rizki <95474427+Rizki008@users.noreply.github.com>
2022-03-16 20:27:07 +0700

    menambahkan file member untuk penjualan detail

---
+++ resources/views/penjualan_detail/member.blade.php
@@ -0,0 +1,39 @@
+<div class="modal fade" id="modal-member" aria-describedby="modal-member">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Pilih Member</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-striped table-bordered table-member">
+                    <thead>
+                        <th width="5%">No</th>
+                        <th>Nama</th>
+                        <th>Telepon</th>
+                        <th>Alamat</th>
+                        <th><i class="fa fa-cog"></i></th>
+                    </thead>
+                    <tbody>
+                        @foreach ($member as $key => $item)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $item->nama }}</td>
+                                <td>{{ $item->telepon }}</td>
+                                <td>{{ $item->alamat }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-primary btn-sm"
+                                        onclick="pilihMember('{{ $item->id_member }}', '{{ $item->kode_member }}')">
+                                        <i class="fa fa-check"></i>Pilih
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
