d5e3e33bb5e1b7bab7fe641b3655e459656ac9e2
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-03-19 07:18:33 +0700

    menambahkan file view untuk user

---
+++ resources/views/user/form.blade.php
@@ -0,0 +1,44 @@
+<div class="modal fade" id="modal-form" aria-labelledby="modal-form">
+    <div class="modal-dialog">
+        <form action="" method="post" class="form-horizontal">
+            @csrf
+            @method('post')
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title"></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="name">Nama</label>
+                        <input type="text" name="name" id="name" class="form-control" required autofocus>
+                        <span class="help-block wirth-errors"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" class="form-control" required autofocus>
+                        <span class="help-block wirth-errors"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="form-control" required
+                            minlength="8">
+                        <span class="help-block wirth-errors"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation">Konfirmasi Password</label>
+                        <input type="password" name="password_confirmation" id="password_confirmation"
+                            class="form-control" required data-match="#password">
+                        <span class="help-block wirth-errors"></span>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,142 @@
+@extends('layout.master')
+
+@section('title')
+    Data User
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="breadcrumb-item active">User</li>
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <button onclick="addForm('{{ route('user.store') }}')" type="button"
+                            class="btn btn-primary btn-sm"><i class="fa fa-plus"></i>
+                            Add</button>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th width="5%">No</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th width="15%"><i class="fa fa-cog"></i></th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+    </div>
+    @includeIf('user.form')
+@endsection
+
+@push('scripts')
+    <script>
+        let table;
+
+        $(function() {
+            table = $('.table').DataTable({
+                processing: true,
+                autoWidth: false,
+                ajax: {
+                    url: '{{ route('user.data') }}',
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'name'
+                    },
+                    {
+                        data: 'email'
+                    },
+                    {
+                        data: 'aksi',
+                        searchable: false,
+                        sortable: false
+                    }
+                ]
+            });
+
+            $('#modal-form').validator().on('submit', function(e) {
+                if (!e.preventDefault()) {
+                    $.ajax({
+                            url: $('#modal-form form').attr('action'),
+                            type: 'post',
+                            data: $('#modal-form form').serialize()
+                        })
+                        .done((response) => {
+                            $('#modal-form').modal('hide');
+                            table.ajax.reload();
+                        })
+                        .fail((errors) => {
+                            alert('Tidak Dapat Menambahkan Data');
+                            return
+                        });
+                }
+            });
+        });
+
+        function addForm(url) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Tambah Data user');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('post');
+            $('#modal-form [name=name]').focus();
+
+            $('#password, #password_confirmation').attr('required', true);
+        }
+
+        function editForm(url) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Edit Data user');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('put');
+            $('#modal-form [name=name]').focus();
+
+            $('#password, #password_confirmation').attr('required', false);
+            $.get(url)
+                .done((response) => {
+                    $('#modal-form [name=name]').val(response.name);
+                    $('#modal-form [name=email]').val(response.email);
+                })
+                .fail((errors) => {
+                    alert('Tidak Dapat Edit Data');
+                    return;
+                });
+        }
+
+        function deleteData(url) {
+            if (confirm('Apakah Yakin Akan Menghapus Data??')) {
+                $.post(url, {
+                        '_token': $('[name=csrf-token]').attr('content'),
+                        '_method': 'delete'
+                    })
+                    .done((response) => {
+                        table.ajax.reload();
+                    })
+                    .fail((errors) => {
+                        alert('Tidak Dapat Menghapus Data');
+                        return;
+                    });
+            }
+        }
+    </script>
+@endpush
